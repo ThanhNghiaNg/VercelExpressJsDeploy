a7d7fa700b4a56bc5d5878cb05faaafaad1e7665
JTNghia <nghiant089@gmail.com>
2023-03-03 20:00:53 +0700

    update home root

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require("express");
 
 const app = express();
 
+app.use("/", (req, res, next) => {
+  return res.send("Home. Type on the root url '/test' to see another API");
+});
 app.use("/test", (req, res, next) => {
   return res.send({ message: "Successfully Access to Server!" });
 });
